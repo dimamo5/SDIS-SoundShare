46fb900ac6d32901923b34480cd826dfa57a9dcf
Diogo Moura <Diogo Moura>
2016-05-15 00:22:28 +0100

    hotfix in room

--- src/streaming/Room.java
+++ src/streaming/Room.java
@@ -11,9 +11,9 @@ import java.util.Timer;
 /**
  * Created by diogo on 12/05/2016.
  */
-public class Room implements Runnable{
-    private static ServerSocket welcomeSocket;
-    private static final int listenPort = 5000;
+public class Room{
+    private ServerSocket welcomeSocket;
+    private final int listenPort = 5000;
     public static final int FRAMESIZE = 2048;
     private ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
     private Semaphore sem = new Semaphore(1);
@@ -21,46 +21,10 @@ public class Room implements Runnable{
     private double musicSec = 0;
 
     public static void main(String[] args) {
-
+        new Room();
     }
 
-    /*public static void sendFile(File f) {
-        byte[] mybytearray = new byte[FRAMESIZE];
-
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(f);
-        } catch (FileNotFoundException ex) {
-            // Do exception handling
-        }
-
-        int chunks = (int) f.length() / FRAMESIZE;
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        System.out.println("Tamanho ficheiro: " + f.length() + "Dividido em: " + f.length() / FRAMESIZE);
-        System.out.println("Fram per sec: " + f.length() / songTime);
-
-        for (int m = 0; m < chunks; m++) {
-            try {
-                sem.acquire();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                bis.read(mybytearray, 0, FRAMESIZE);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            for (ClientHandler client : clients) {
-                client.send(mybytearray);
-                System.out.println(musicSec);
-            }
-            sem.release();
-        }
-
-    }*/
-
-    @Override
-    public void run() {
+    public Room() {
         //new Converter("resources/batmobile.wav","resources/batmobile.mp3").encodeMP3();
         try {
             welcomeSocket = new ServerSocket(listenPort);
