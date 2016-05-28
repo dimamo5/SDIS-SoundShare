4ca94ceb783a7f51091ea0c68e6a2a2197f4e969
Miguel Botelho <m_botelho8@hotmail.com>
2016-05-28 22:22:57 +0100

    fault tolerance bby

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,62 +1,68 @@
 package server;
 
-import database.Database;
-import player.Track;
 import streaming.Room;
 
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 import java.io.*;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Map;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.*;
 
 public class Server implements Runnable {
     private Hashtable<Integer, Room> rooms = new Hashtable<>();
     private final int ssl_port = 9000;
+    private final String MSG = "AYY CAPTAIN";
+    private DatagramSocket channel;
     private SSLServerSocket sslserversocket = null;
+
 
     public static void main(String[] args) {
         Server s = Singleton.getInstance().getServer();
-
         Thread msgHandling = new Thread(s);
         msgHandling.start();
         s.newRoom(Room.DEFAULTPORT);
     }
 
-    public String roomsToString() {
-        String[] args = new String[rooms.size() * 2];
-        int i = 0;
 
-        Iterator it = rooms.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            System.out.println(pair.getKey() + " = " + pair.getValue());
-            Room r = (Room) pair.getValue();
-
-            args[i] = String.valueOf(r.getPort());
-            Track t = r.getPlaylist().getCurrentTrack();
-            if (t == null)
-                args[i + 1] = "No music playing";
-            else args[i + 1] = t.getInfo().getTitle();
-            i = i + 2;
+    public Server(int port) {
+        try {
+            channel = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
-        return args.toString();
-    }
 
-    public Server() {
+        //CONNECT TO EXTERN SERVICE
+        new Thread() {
+            public void run() {
+                while (true) {
+                    byte[] buf = new byte[256];
+                    DatagramPacket received = new DatagramPacket(buf, buf.length);
+                    try {
+                        channel.receive(received);
+                        System.out.println("Got a ping from external service: "+new String (received.getData()));
+                        DatagramPacket sent = new DatagramPacket(MSG.getBytes(), MSG.getBytes().length, received.getAddress(), received.getPort());
+                        channel.send(sent);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+
         System.setProperty("javax.net.ssl.keyStore", "keystore");
         System.setProperty("javax.net.ssl.keyStorePassword", "123456");
         SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+
         try {
             sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(this.ssl_port);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
+
 
     @Override
     public void run() {
